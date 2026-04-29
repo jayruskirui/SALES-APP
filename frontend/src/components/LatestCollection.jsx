078d29a9b24e8cceea75c7bc32ee9812afd836da
JAYRUS KIPROTICH <jayruskirui@gmail.com>
2026-04-29 20:38:59 +0300

    ProductItem

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -1,10 +1,16 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../context/ShopContext'
 import Title from './Title'
+import ProductItem from './ProductItem'
 
 const LatestCollection = () => {
 
     const { products} = useContext(ShopContext)
+    const [latestproducts, setlatestproducts] = useState([])
+
+    useEffect(() => {
+        setlatestproducts(products.slice(0, 10))
+    }, [])
 
   return (
     <div className='my-10'>
@@ -13,6 +19,16 @@ const LatestCollection = () => {
             <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-500'>
                 Discover our latest collection of products designed to meet your needs and exceed your expectations.
             </p>
+        </div>
+
+        {/* Rendering products */}
+        <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
+            {
+                latestproducts.map((item,index) => (
+                    <ProductItem key={index} id={item._id} image={item.image} name={item.name} price={item.price} />
+                ))
+            }
+
         </div>
 
     </div>
