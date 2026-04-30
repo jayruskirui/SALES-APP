a65623c1efa3d3d9a232508564352bb13a16a72e
JAYRUS KIPROTICH <jayruskirui@gmail.com>
2026-04-30 14:26:13 +0300

    Rendering products

--- frontend/src/components/BestSeller.jsx
+++ frontend/src/components/BestSeller.jsx
@@ -1,5 +1,8 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../context/ShopContext'
+import Title from './Title';
+import Product from '../pages/Product';
+import ProductItem from './ProductItem';
 
 const BestSeller = () => {
 
@@ -7,13 +10,26 @@ const BestSeller = () => {
   const [bestSeller, setbestSeller] = useState([]);
 
   useEffect(() => {
-      const bestProduct = products.filter((item) => (item.bestSeller));
+      const bestProduct = products.filter((item) => (item.bestseller));
       setbestSeller(bestProduct.slice(0, 5));
   }, []);
 
   return (
-    <div>
-        
+    <div className='my-10'>
+      <div className='text-center text-3xl py-8'>
+        <Title text1={'BEST'} text2={'SELLERS'} />
+        <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
+          Discover our top-performing products that customers love!
+        </p>
+      </div>
+
+      <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
+        {
+          bestSeller.map((item, index) => (
+            <ProductItem key={index} id={item._id} image={item.image} name={item.name} price={item.price}/>
+          ))
+        }
+      </div>   
     </div>
   )
 }
