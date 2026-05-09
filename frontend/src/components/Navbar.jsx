e8c36b8a6264bbd7c97ea9bd962305c2aebf9735
JAYRUS KIPROTICH <jayruskirui@gmail.com>
2026-05-09 10:01:29 +0300

    Formarting of get count

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
 
     const [visible, setVisible] = useState(false)
 
-    const { setShowSearch } = useContext(ShopContext);
+    const { setShowSearch, getCartCount } = useContext(ShopContext);
 
   return (
     <div className="flex items-center justify-between py-5 font-medium">
@@ -55,7 +55,7 @@ const Navbar = () => {
             <Link to='/cart' className="relative">
               <img src={assets.cart_icon} alt=" " className="w-5 min-w-5"/>
               <p className="absolute right-[-5px] bottom-[-5px] w-4 h-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[12px]">
-                10
+                {getCartCount()}
               </p>
             </Link>
             <img onClick={() => setVisible(true)} src={assets.menu_icon} alt=" " className="w-5 cursor-pointer sm:hidden"/>
--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -2,11 +2,12 @@ import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { ShopContext } from '../context/ShopContext';
 import { assets } from '../assets/assets';
+import RelatedProducts from '../components/RelatedProducts';
 
 const Product = () => {
 
 const { productId } = useParams();
-const { products, currency } = useContext(ShopContext);
+const { products, currency, addToCart } = useContext(ShopContext);
 const [productData, setProductData] = useState (false);
 const [image, setImage] = useState('');
 const [size, setSize] = useState('');
@@ -17,7 +18,6 @@ const fetchproductData = async () => {
     if (item._id === productId){
       setProductData(item);
       setImage(item.image[0]);
-      console.log(item);
       return null;
     }
   })
@@ -68,7 +68,7 @@ useEffect(() => {
                 ))}
               </div>
             </div>
-            <button className='bg-black text-white px-8 py-3 text-sm active:bg-gray-700 cursor-pointer'>ADD TO CART</button>
+            <button onClick={() => addToCart(productData._id, size)} className='bg-black text-white px-8 py-3 text-sm active:bg-gray-700 cursor-pointer'>ADD TO CART</button>
             <hr className='mt-8 sm:w-4/5'/>
             <div className='text-sm text-gray-500 mt-5 flex flex-col gap-1'>
                 <p>100% Original product.</p>
@@ -89,6 +89,10 @@ useEffect(() => {
           <p>E-commerce websites typically display products or services along with detailed descriptions, images, prices, and any available variations (e.g., sizes, colors). Each product usually has its own dedicated page with relevant information.</p>
         </div>
       </div>
+
+      {/* ----------------Display Related Products---------------- */}
+      <RelatedProducts category={productData.category} subCategory={productData.subCategory}/>
+
     </div>
   ) : <div className='opacity-0'></div>
 }
